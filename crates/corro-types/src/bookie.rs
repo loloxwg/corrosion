e50317cb917c1583f912c540600a048d004cea31
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-06-01 18:05:05 +0100

    fix up metrics
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- crates/corro-types/src/bookie.rs
+++ crates/corro-types/src/bookie.rs
@@ -875,7 +875,7 @@ impl BookieDbParams {
             let len = self.complete_version_deletes.len();
             if count != len {
                 warn!("did not delete some complete versions from db, expected {len}, got {count}");
-                let details = json!({"count": count, "expected": len, version});
+                let details = json!({"count": count, "expected": len});
                 assert_unreachable!("ineffective deletion of complete versions in-db", &details);
             }
         }
