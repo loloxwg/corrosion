c11fa82c22e49d7a535e22b88363009e83715f20
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-06-02 14:06:01 +0100

    make broadcast method optional
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- crates/corro-agent/src/api/peer/mod.rs
+++ crates/corro-agent/src/api/peer/mod.rs
@@ -2424,6 +2424,7 @@ mod tests {
             max_mtu: None,
             disable_gso: false,
             member_id: None,
+            broadcast_method: corro_types::config::BroadcastMethod::Gossip,
         };
 
         let server = gossip_server_endpoint(&gossip_config).await?;
